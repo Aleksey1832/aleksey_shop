a0d344ce04cd56f1efbf0f5f98bfef6280d10ab8
Aleksey <alekseykhitrov@gmail.com>
2024-09-01 19:26:53 +0300

    01.09.24

--- shop/urls.py
+++ shop/urls.py
@@ -15,4 +15,9 @@ urlpatterns = [
         views.product_list,
         name='product_list_by_category'
     ),
+    path(
+        '<int:id>/<slug:slug>/',
+        views.product_detail,
+        name='product_detail'
+    ),
 ]
