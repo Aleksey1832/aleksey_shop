c2ce2310fcc739ac159714dc6e98416f816b612b
Aleksey <alekseykhitrov@gmail.com>
2025-04-01 19:23:42 +0300

    07.05.25 settings

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -13,10 +13,10 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG')
 
 RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
-ALLOWED_HOSTS = ['178.250.246.96', 'localhost']
+ALLOWED_HOSTS = ['5dd5-141-94-16-245.ngrok-free.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -177,8 +177,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
