2dbdc99de2341b0f80a76f18c784d150db188d2e
Aleksey <alekseykhitrov@gmail.com>
2025-04-28 19:07:19 +0300

    28.04.2025

--- djangoProject1/celery.py
+++ djangoProject1/celery.py
@@ -6,3 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject1.settings')
 app = Celery('djangoProject1')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+#
