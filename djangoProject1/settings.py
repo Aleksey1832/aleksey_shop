bee4a52fe91d6ff8ef97441c01cfb7cc2a9f1093
Aleksey <alekseykhitrov@gmail.com>
2025-05-07 20:45:40 +0300

    07.05.2025 settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -13,10 +13,10 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG')
 
 RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
-ALLOWED_HOSTS = ['5dd5-141-94-16-245.ngrok-free.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['178.250.246.96', 'localhost']
 
 
 # Application definition
@@ -177,7 +177,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -202,9 +203,9 @@ EMAIL_HOST_USER = os.getenv('BACKEND_EMAIL_HOST_USER')  # Ваш логин на
 EMAIL_HOST_PASSWORD = os.getenv('BACKEND_EMAIL_HOST_PASSWORD')  # Ваш пароль от почтового ящика (Django_project1)
 DEFAULT_FROM_EMAIL = os.getenv('BACKEND_DEFAULT_FROM_EMAIL')  # Адрес, с которого будут отправляться письма
 
-""" Хранит сообщения в сессии """
 MASSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-""" RECAPTCHA """
 RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
+
+
