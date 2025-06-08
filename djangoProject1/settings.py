90fe2329748aa156c935b4f63103f92c5c0f832b
Aleksey <alekseykhitrov@gmail.com>
2025-06-08 10:06:02 +0300

    08.06.2025 update settings.py, db.json

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -37,7 +37,7 @@ RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
 """ localhost """
 # ALLOWED_HOSTS = ['5dd5-141-94-16-245.ngrok-free.app', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 """ Deploy """
 # ALLOWED_HOSTS = ['178.250.246.96', 'localhost']
@@ -119,14 +119,14 @@ DATABASES = {
 CACHES = {
     'default': {  # храним сессии пользователей
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': f'redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB_DEFAULT")}',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     },
     'views': {  # храним просмотры пользователей
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/10',
+        'LOCATION': f'redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB_VIEWS")}',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
@@ -169,7 +169,11 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_REDIRECT_URL_HTTPS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://5dd5-141-94-16-245.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://5dd5-141-94-16-245.ngrok-free.app',
+    'http://localhost',
+    'http://web'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -186,10 +190,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-""" localhost """
-# STATIC_URL = 'static/'
-
-""" deploy """
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
@@ -203,7 +203,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 7
+SESSION_COOKIE_AGE = (int(os.getenv('COOKIE_SECONDS')) *
+                      int(os.getenv('COOKIE_MINUTES')) *
+                      int(os.getenv('COOKIE_HOURS')) *
+                      int(os.getenv('COOKIE_DAYS')))
 
 CART_SESSION_ID = 'cart'
 
