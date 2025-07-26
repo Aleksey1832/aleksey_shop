164fba4e64a46a1f7fab4d372589522658a94812
Aleksey <alekseykhitrov@gmail.com>
2025-07-26 17:08:43 +0300

    26.07.2025 настройка celery

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,8 +1,19 @@
 from celery import shared_task
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from orders.models import Order
 from orders.pdf import generate_pdf
+
+
+@shared_task
+def sent_email_after_order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}'
+    message = (f'Уважаемый(ая), {order.first_name}!\n'
+               f'Ваш заказ № {order_id} успешно оформлен!\n'
+               f'На сумму {order.get_total_cost()}')
+    mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
+    return mail_sent
 
 
 @shared_task
@@ -10,15 +21,18 @@ def order_created(order_id):
     order = Order.objects.get(id=order_id)
     pdf_file = generate_pdf(order)
     email = EmailMessage(
-        subject=f'Ваш заказ от {order.created_at.strftime("%d.%m.%Y")}.',
-        body=f'Спасибо за покупку {order.first_name}! В приложении информация о Вашем заказе.',
+        subject=f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}',
+        body=f'Уважаемый(ая), {order.first_name}!\n'
+             f'Ваш заказ № {order_id} успешно оформлен!\n'
+             f'На сумму {order.get_total_cost()}',
         from_email=settings.EMAIL_HOST_USER,
         to=[order.email]
     )
 
     email.attach(
-        f'order_{order.id}_{order.created_at.strftime("%d.%m.%Y")}.pdf',
-        pdf_file,
-        'application/pdf'
+        filename=f'order_{order.id}_{order.created_at.strftime("%d.%m.%Y")}.pdf',
+        content=pdf_file.getvalue(),
+        mimetype='application/pdf'
     )
+    pdf_file.close()
     email.send()
