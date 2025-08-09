f16bbf8ae43443410e7223c2f310b7df3092778f
Aleksey <alekseykhitrov@gmail.com>
2025-08-09 12:12:19 +0300

    09.08.2025 tasks mail

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,19 +1,19 @@
 from celery import shared_task
 from django.conf import settings
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import EmailMessage
 from orders.models import Order
 from orders.pdf import generate_pdf
 
 
-@shared_task
-def sent_email_after_order_created(order_id):
-    order = Order.objects.get(id=order_id)
-    subject = f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}'
-    message = (f'Уважаемый(ая), {order.first_name}!\n'
-               f'Ваш заказ № {order_id} успешно оформлен!\n'
-               f'На сумму {order.get_total_cost()}')
-    mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
-    return mail_sent
+#@shared_task   - , send_mail
+#def sent_email_after_order_created(order_id):
+#    order = Order.objects.get(id=order_id)
+#    subject = f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}'
+#    message = (f'Уважаемый(ая), {order.first_name}!\n'
+#               f'Ваш заказ № {order_id} успешно оформлен!\n'
+#               f'На сумму {order.get_total_cost()}')
+#    mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
+#    return mail_sent
 
 
 @shared_task
