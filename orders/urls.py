dcec26edcc5d33660cdd61b755821d2b63187de6
Aleksey <alekseykhitrov@gmail.com>
2025-10-01 22:23:53 +0300

    01.10.25-yookassa_webhook

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('success/<int:order_id>/', views.order_success, name='order_success'),
     path('payment/<int:order_id>', views.payment, name='payment'),
     path('cancel/<int:order_id>', views.cancel_order, name='cancel_order'),
+    path('pay/', views.yookassa_webhook, name='yookassa_webhook'),
 ]
