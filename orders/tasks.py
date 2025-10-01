a56bdc9a59ad923e3709f0072d8359478a3155b0
Aleksey <alekseykhitrov@gmail.com>
2025-10-01 22:18:24 +0300

    01.10.25-2FA check_code

--- orders/tasks.py
+++ orders/tasks.py
@@ -5,15 +5,15 @@ from orders.models import Order
 from orders.pdf import generate_pdf
 
 
-#@shared_task   - , send_mail
-#def sent_email_after_order_created(order_id):
-#    order = Order.objects.get(id=order_id)
-#    subject = f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}'
-#    message = (f'Уважаемый(ая), {order.first_name}!\n'
-#               f'Ваш заказ № {order_id} успешно оформлен!\n'
-#               f'На сумму {order.get_total_cost()}')
-#    mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
-#    return mail_sent
+# @shared_task
+# def order_created(order_id):
+#     order = Order.objects.get(id=order_id)
+#     subject = f'Заказ № {order_id} от {order.created_at.strftime("%d.%m.%Y")}'
+#     message = (f'Уважаемый(ая), {order.first_name}!\n'
+#                f'Ваш заказ № {order_id} успешно оформлен!\n'
+#                f'На сумму {order.get_total_cost()}')
+#     mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
+#     return mail_sent
 
 
 @shared_task
